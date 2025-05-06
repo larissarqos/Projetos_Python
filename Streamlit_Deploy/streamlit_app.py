9ddf1e0ad5e362d604c11393f3cb8ca46b1b7954
Larissa <larissarqos17@gmail.com>
2025-05-06 19:20:02 -0300

    Update streamlit_app.py

--- Streamlit_Deploy/streamlit_app.py
+++ Streamlit_Deploy/streamlit_app.py
@@ -7,7 +7,30 @@ st.title('Machine Learning App')
 
 st.info('Esse aplicativo construiu um modelo de machine learning!')
 
-with st.expander('Data'):
+# Exibindo dados
+with st.expander('Dados'):
   st.write('**Dados Brutos**')
   df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
   df
+
+st.write('**X**')
+
+# todos os dados, com exceção da variável target
+X = df.drop('species', axis=1)
+X
+
+# variável target
+st.write('**y**')
+y = df.species
+
+# Criando gráfico
+with st.expander('Visualização dos dados')
+st.scatter_chart(data=df,
+                x='bill_length_mm',
+                y='body_mass_g'
+                color='species')
+
+
+
+
+
