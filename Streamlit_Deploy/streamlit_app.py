1267d3aa5fd0ef6b916d3a4146521872c3108761
Larissa <larissarqos17@gmail.com>
2025-05-06 18:41:11 -0300

    Update streamlit_app.py

--- Streamlit_Deploy/streamlit_app.py
+++ Streamlit_Deploy/streamlit_app.py
@@ -1,5 +1,13 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 
 st.title('Machine Learning App')
 
-st.write('🌎 Hello world!')
+st.info('Esse aplicativo construiu um modelo de machine learning!')
+
+with st.expander('Data'):
+  st.write('**Dados Brutos**')
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+  df
