71535af2775ab28b50adbd57df662c534f59eec3
Larissa <larissarqos17@gmail.com>
2025-05-06 19:50:02 -0300

    Update streamlit_app.py

--- Streamlit_Deploy/streamlit_app.py
+++ Streamlit_Deploy/streamlit_app.py
@@ -36,10 +36,21 @@ with st.sidebar:
   gender = st.selectbox('Gênero', ('male', 'female'))
   # em bill_length_mm estão os valores máximo, mínimo e médio
   bill_length_mm = st.slider('Comprimento do Bico (mm)', 32.1, 59.6, 43.9)
-  bill_depth_mm = st.slider('Bill depth (mm)', 13.1, 21.5, 17.2)
-  flipper_length_mm = st.slider('Flipper length (mm)', 172.0, 231.0, 201.0)
-  body_mass_g = st.slider('Body mass (g)', 2700.0, 6300.0, 4207.0)
+  bill_depth_mm = st.slider('Profundidade do Bico (mm)', 13.1, 21.5, 17.2)
+  flipper_length_mm = st.slider('Comprimento da Nadadeira (mm)', 172.0, 231.0, 201.0)
+  body_mass_g = st.slider('Massa Corporal (g)', 2700.0, 6300.0, 4207.0)
 
+  # Criando um dataframe para inserção dos dados
+  data = {'island': island,
+            'sex': gender,
+            'bill_length_mm': bill_length_mm,
+            'bill_depth_mm': bill_depth_mm,
+            'flipper_length_mm': flipper_length_mm,
+            'body_mass_g': body_mass_g}
+  input_df = pd.DataFrame(data, index=[0])
+  input_pinguins = pd.concat([input_df, X_raw], axis=0)
+
+input_df
 
   
 
