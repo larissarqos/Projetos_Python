75d9827df4f977ce26ad80eaa537827bb8108343
Larissa <larissarqos17@gmail.com>
2025-05-06 19:53:21 -0300

    Update streamlit_app.py

--- Streamlit_Deploy/streamlit_app.py
+++ Streamlit_Deploy/streamlit_app.py
@@ -48,7 +48,7 @@ with st.sidebar:
             'flipper_length_mm': flipper_length_mm,
             'body_mass_g': body_mass_g}
   input_df = pd.DataFrame(data, index=[0])
-  input_pinguins = pd.concat([input_df, X], axis=0)
+  input_penguins = pd.concat([input_df, X], axis=0)
 
 input_df
 
