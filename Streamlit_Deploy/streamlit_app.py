1d56676a98c551b2211bae34bdb6adb067ddef86
Larissa <larissarqos17@gmail.com>
2025-05-06 20:54:20 -0300

    Update streamlit_app.py

--- Streamlit_Deploy/streamlit_app.py
+++ Streamlit_Deploy/streamlit_app.py
@@ -50,7 +50,11 @@ with st.sidebar:
   input_df = pd.DataFrame(data, index=[0])
   input_penguins = pd.concat([input_df, X], axis=0)
 
-input_df
+with st.expander('Informações inseridas neste mês')
+  st.write('**Dados inseridos**')
+  input_df
+  st.write('**Dados combinados**')
+  input_penguins
 
   
 
