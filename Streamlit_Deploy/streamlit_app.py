fe9fb097cddb4d7e8450729486e847f7612812be
Larissa <larissarqos17@gmail.com>
2025-05-06 19:37:10 -0300

    Update streamlit_app.py

--- Streamlit_Deploy/streamlit_app.py
+++ Streamlit_Deploy/streamlit_app.py
@@ -36,6 +36,10 @@ with st.sidebar:
   gender = st.selectbox('Gênero', ('male', 'female'))
   # em bill_length_mm estão os valores máximo, mínimo e médio
   bill_length_mm = st.slider('Comprimento do Bico (mm)', 32.1, 59.6, 43.9)
+  bill_depth_mm = st.slider('Bill depth (mm)', 13.1, 21.5, 17.2)
+  flipper_length_mm = st.slider('Flipper length (mm)', 172.0, 231.0, 201.0)
+  body_mass_g = st.slider('Body mass (g)', 2700.0, 6300.0, 4207.0)
+  gender = st.selectbox('Gender', ('male', 'female'))
 
 
   
