d9bd9b72cec9a6369d385a8e7d504f06d07e1394
Larissa <larissarqos17@gmail.com>
2025-05-06 21:09:21 -0300

    Update streamlit_app.py

--- Streamlit_Deploy/streamlit_app.py
+++ Streamlit_Deploy/streamlit_app.py
@@ -1,60 +1,55 @@
+
 import streamlit as st
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 
-st.title('Machine Learning App')
+st.title('🤖 Machine Learning App')
 
-st.info('Esse aplicativo construiu um modelo de machine learning!')
+st.info('This is app builds a machine learning model!')
 
-# Exibindo dados
-with st.expander('Dados'):
-  st.write('**Dados Brutos**')
+with st.expander('Data'):
+  st.write('**Raw data**')
   df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
   df
 
-# Todos os dados, com exceção da variável target (variáveis preditoras)
-with st.expander('X'):
-  st.write('**Variáveis Preditoras**')
-  X = df.drop('species', axis=1)
-  X
+  st.write('**X**')
+  X_raw = df.drop('species', axis=1)
+  X_raw
 
-# Variável target
-with st.expander('y'):
-  st.write('**Variável Target**')
-  y = df.species
-  y
+  st.write('**y**')
+  y_raw = df.species
+  y_raw
 
-# Criando gráfico
-with st.expander('Visualização dos dados'):
+with st.expander('Data visualization'):
   st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
 
-# Preparando dados
+# Input features
 with st.sidebar:
-  st.header('Insira os dados do pinguim')
-  island = st.selectbox('Ilha', ('Biscoe', 'Dream', 'Torgersen'))
-  gender = st.selectbox('Gênero', ('male', 'female'))
-  # em bill_length_mm estão os valores máximo, mínimo e médio
-  bill_length_mm = st.slider('Comprimento do Bico (mm)', 32.1, 59.6, 43.9)
-  bill_depth_mm = st.slider('Profundidade do Bico (mm)', 13.1, 21.5, 17.2)
-  flipper_length_mm = st.slider('Comprimento da Nadadeira (mm)', 172.0, 231.0, 201.0)
-  body_mass_g = st.slider('Massa Corporal (g)', 2700.0, 6300.0, 4207.0)
-
-  # Criando um dataframe para inserção dos dados
+  st.header('Input features')
+  island = st.selectbox('Island', ('Biscoe', 'Dream', 'Torgersen'))
+  bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 43.9)
+  bill_depth_mm = st.slider('Bill depth (mm)', 13.1, 21.5, 17.2)
+  flipper_length_mm = st.slider('Flipper length (mm)', 172.0, 231.0, 201.0)
+  body_mass_g = st.slider('Body mass (g)', 2700.0, 6300.0, 4207.0)
+  gender = st.selectbox('Gender', ('male', 'female'))
+  
+  # Create a DataFrame for the input features
   data = {'island': island,
-            'sex': gender,
-            'bill_length_mm': bill_length_mm,
-            'bill_depth_mm': bill_depth_mm,
-            'flipper_length_mm': flipper_length_mm,
-            'body_mass_g': body_mass_g}
+          'bill_length_mm': bill_length_mm,
+          'bill_depth_mm': bill_depth_mm,
+          'flipper_length_mm': flipper_length_mm,
+          'body_mass_g': body_mass_g,
+          'sex': gender}
   input_df = pd.DataFrame(data, index=[0])
-  input_penguins = pd.concat([input_df, X], axis=0)
+  input_penguins = pd.concat([input_df, X_raw], axis=0)
 
-with st.expander('Informações inseridas'):
-  st.write('**Dados inseridos**')
+with st.expander('Input features'):
+  st.write('**Input penguin**')
   input_df
-  st.write('**Dados combinados**')
+  st.write('**Combined penguins data**')
   input_penguins
+
 
 # Data preparation
 # Encode X
@@ -64,9 +59,64 @@ df_penguins = pd.get_dummies(input_penguins, prefix=encode)
 X = df_penguins[1:]
 input_row = df_penguins[:1]
 
+# Encode y
+target_mapper = {'Adelie': 0,
+                 'Chinstrap': 1,
+                 'Gentoo': 2}
+def target_encode(val):
+  return target_mapper[val]
+
+y = y_raw.apply(target_encode)
+
+with st.expander('Data preparation'):
+  st.write('**Encoded X (input penguin)**')
+  input_row
+  st.write('**Encoded y**')
+  y
 
 
-  
+# Model training and inference
+## Train the ML model
+clf = RandomForestClassifier()
+clf.fit(X, y)
+
+## Apply model to make predictions
+prediction = clf.predict(input_row)
+prediction_proba = clf.predict_proba(input_row)
+
+df_prediction_proba = pd.DataFrame(prediction_proba)
+df_prediction_proba.columns = ['Adelie', 'Chinstrap', 'Gentoo']
+df_prediction_proba.rename(columns={0: 'Adelie',
+                                 1: 'Chinstrap',
+                                 2: 'Gentoo'})
+
+# Display predicted species
+st.subheader('Predicted Species')
+st.dataframe(df_prediction_proba,
+             column_config={
+               'Adelie': st.column_config.ProgressColumn(
+                 'Adelie',
+                 format='%f',
+                 width='medium',
+                 min_value=0,
+                 max_value=1
+               ),
+               'Chinstrap': st.column_config.ProgressColumn(
+                 'Chinstrap',
+                 format='%f',
+                 width='medium',
+                 min_value=0,
+                 max_value=1
+               ),
+               'Gentoo': st.column_config.ProgressColumn(
+                 'Gentoo',
+                 format='%f',
+                 width='medium',
+                 min_value=0,
+                 max_value=1
+               ),
+             }, hide_index=True)
 
 
-
+penguins_species = np.array(['Adelie', 'Chinstrap', 'Gentoo'])
+st.success(str(penguins_species[prediction][0]))
